87fa69e9f6d79ed77c8f47b764b43562cee85026
Jerry Ryle <jerry@baton.io>
2026-05-30 13:38:22 -0700

    Clamp assignments to avoid 00.flac and add tests

---
+++ src/clamp.c
@@ -0,0 +1,12 @@
+#include "clamp.h"
+
+uint32_t clamp(uint32_t value, uint32_t min, uint32_t max)
+{
+	if (value < min) {
+		return min;
+	}
+	if (value > max) {
+		return max;
+	}
+	return value;
+}
